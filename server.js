f7abfe0bd5ea23798e3c9da56d1fb691c81b0481
mai <mai.yusuf28@yahoo.com>
2019-11-18 17:56:24 +0200

    feature 4 done

---
+++ server.js
@@ -0,0 +1,63 @@
+'use strict'
+
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT;
+
+const server = express();
+
+// server.use(cors());
+
+
+
+
+
+server.get('/location', (request, response) => {
+    const locationData = require('./data/geo.json');
+    const location = new Location(locationData);
+    response.status(200).json(location);
+  });
+  
+  function Location( data ) {
+      this.search_query = 'lynnwood';
+      this.formatted_query = data.results[0].formatted_address;
+      this.latitude = data.results[0].geometry.location.lat;
+    this.longitude = data.results[0].geometry.location.lng;
+}
+
+server.get('/weather', weatherHanddler);
+
+function weatherHanddler(req,res) {
+    let weatherData = getWeather(req.query.data);
+    res.status(200).json(weatherData);
+}
+
+
+function getWeather (city) {
+    let data = require('./data/darksky.json');
+    
+    return data.daily.data.map( (day) => {
+        return new Weather(day);
+    })
+
+ 
+  };
+  
+  function Weather( day ) {
+
+      this.forecast = day.summary;
+      this.time = new Date(day.time * 1000).toDateString();
+          // this.latitude = data.latitude
+          // this.longitude = data.longitude
+          
+          
+        }
+        server.get('*',(request,Response) =>{
+            Response.status(500).send('Sorry, something went wrong');
+        });
+        
+        server.listen(PORT, () => console.log(`App listening on ${PORT}`));
+        
