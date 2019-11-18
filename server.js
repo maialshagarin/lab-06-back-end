3625eeabcad36c2db88ae77612369569ded5e9b2
Ramzibawayza <ramzibawayza12@gmail.com>
2019-11-18 13:33:24 +0200

    feature 2

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 'use strict'
-
-
 require('dotenv').config();
 const express = require('express');
+
 const cors = require('cors');
 
 const PORT = process.env.PORT;
 
 const server = express();
-
 console.log(server)
+
+server.use(cors());
+
+server.get('/',(request,Response) =>{
+   Response.status(200).send('great job');
+})
+
+server.get('/location', (request, response) => {
+   const locationData = require('./data/geo.json');
+   const location = new Location(locationData);
+   response.status(200).json(location);
+ });
+
+ function Location( data ) {
+   this.search_query = 'lynnwood';
+   this.formatted_query = data.results[0].formatted_address;
+   this.latitude = data.results[0].geometry.location.lat;
+   this.longitude = data.results[0].geometry.location.lng;
+ }
+ 
+server.listen(PORT, () => console.log(`App listening on ${PORT}`));
